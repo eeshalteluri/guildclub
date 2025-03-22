c9ac95f8c5790bbc1e3167a61469e9928ffa6dc8
eeshalteluri <eeshalteluri@gmail.com>
2025-03-22 11:29:02 +0530

    Made the application a PWA

--- frontend/v1/src/app/layout.tsx
+++ frontend/v1/src/app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <meta name="apple-mobile-web-app-title" content="Guildclub" />
+      </head>
       <body className={`${poppins.className} antialiased`}>
           <div className="h-full">
             {children}
